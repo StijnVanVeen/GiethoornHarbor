c99f591e756f52889b34e735852b6d4caa59b167
lars <ljaw.jongenelen@student.avans.nl>
2023-05-26 14:47:57 +0200

    Updated Commands

--- src/SecurityManagementAPI/SecurityManagementAPI/Commands/AddShip.cs
+++ src/SecurityManagementAPI/SecurityManagementAPI/Commands/AddShip.cs
@@ -1,16 +1,20 @@
-﻿namespace SecurityManagementAPI.Commands
+﻿using Infrastructure.Messaging;
+
+namespace SecurityManagementAPI.Commands
 {
 	public class AddShip : Command
 	{
 		public readonly int Id;
 		public readonly DateTime ArrivalDate;
 		public readonly DateTime? DepartureDate;
+		public readonly int DockId;
 
-		public AddShip(Guid messageId, int id, DateTime arrivalDate, DateTime? departureDate) : base(messageId)
+		public AddShip(Guid messageId, int id, DateTime arrivalDate, DateTime? departureDate, int dockid) : base(messageId)
 		{
 			Id = id;
 			ArrivalDate = arrivalDate;
 			DepartureDate = departureDate;
+			Dockid = dockid;
 		}
 	}
 }
--- src/SecurityManagementAPI/SecurityManagementAPI/Commands/AddTruck.cs
+++ src/SecurityManagementAPI/SecurityManagementAPI/Commands/AddTruck.cs
@@ -1,17 +1,17 @@
-﻿namespace SecurityManagementAPI.Commands
+﻿using Infrastructure.Messaging;
+
+namespace SecurityManagementAPI.Commands
 {
 	public class AddTruck : Command
 	{
 		public readonly int Id;
 		public readonly DateTime ArrivalDate;
-		public readonly DateTime? DepartureDate;
 		public readonly int ShipId;
 
-		public AddTruck(Guid messageId, int id, DateTime arrivalDate, DateTime? departureDate, int shipId) : base(messageId)
+		public AddTruck(Guid messageId, int id, DateTime arrivalDate, int shipId) : base(messageId)
 		{
 			Id = id;
 			ArrivalDate = arrivalDate;
-			DepartureDate = departureDate;
 			ShipId = shipId;
 		}
 	}
